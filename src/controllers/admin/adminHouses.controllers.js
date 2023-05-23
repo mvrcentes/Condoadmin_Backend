8f8f9725636a072cc6883cb23cf9e3c5f7d0a5de
Javier Chavez <77762186+jannisce@users.noreply.github.com>
2023-05-22 21:35:55 -0600

    cambios jeje

--- src/controllers/admin/adminHouses.controllers.js
+++ src/controllers/admin/adminHouses.controllers.js
@@ -1,4 +1,6 @@
 import supabase from '../../database.js'
+import House from '../../models/House.js'
+import Residente from '../../models/Residente.js'
 
 export const getHouses = async (req, res) => {
     const { data, error } = await supabase
@@ -10,7 +12,7 @@ export const getHouses = async (req, res) => {
     return res.json(data)
 }
 
-export const getHouseByID = async (req, res) => {
+export const getResidentsByHouseID = async (req, res) => {
     const { id } = req.params
 
     const { data, error } = await supabase
@@ -21,4 +23,89 @@ export const getHouseByID = async (req, res) => {
     if (error) return res.json({ error: error.message })
 
     return res.json(data)
+}
+
+export const createHouse = async (req, res) => {
+    const { num_casa, direccion, condominio, cuota_mensual } =
+        req.body
+
+    //Validacion de datos
+    const { error, value } = House.validate({
+        num_casa, 
+        direccion, 
+        condominio, 
+        cuota_mensual
+    })
+
+    if (error) {
+        console.log(error.message)
+        return res.json({ error: error.message })
+    }
+
+    try {
+        // Insert the new user into the medico table
+        const { data, error } = await supabase
+            .from('casa')
+            .insert({
+                num_casa: num_casa, 
+                direccion: direccion, 
+                condominio: condominio,
+                cuota_mensual: cuota_mensual,
+            });
+            
+
+        if (error) {
+            console.error(error.message);
+            return res.json({ error: error.message });
+        }
+
+        // Return a success message if the insert was successful
+        console.log(data);
+        res.json({ message: 'Casa creado' });
+    } catch (error) {
+        console.error(error.message);
+        return res.json({ error: error.message });
+    }
+}
+
+export const createResident = async (req, res) => {
+    const { num_casa, cui, nombre, telefono, correo, tipo_residente } =
+        req.body
+
+    //Validacion de datos
+    const { error, value } = Residente.validate({
+        num_casa, cui, nombre, telefono, correo, tipo_residente
+    })
+
+    if (error) {
+        console.log(error.message)
+        return res.json({ error: error.message })
+    }
+
+    try {
+        // Insert the new user into the medico table
+        const { data, error } = await supabase
+            .from('residente')
+            .insert({
+                casa:           num_casa,
+                cui:            cui,
+                nombre:         nombre,
+                telefono:       telefono,
+                correo:         correo,
+                tipo_residente: tipo_residente
+            });
+            
+
+        if (error) {
+            console.error(error.message);
+            return res.json({ error: error.message });
+        }
+
+        // Return a success message if the insert was successful
+        console.log(data);
+        res.json({ message: 'Residente creado' });
+    } catch (error) {
+        console.error(error.message);
+        return res.json({ error: error.message });
+    }
 }
--- src/routes/adminHouses.routes.js
+++ src/routes/adminHouses.routes.js
@@ -1,11 +1,16 @@
 import { Router } from "express"
 
-import { getHouses, getHouseByID } from "../controllers/admin/adminHouses.controllers.js"
+import { getHouses, getResidentsByHouseID, createHouse, createResident } from "../controllers/admin/adminHouses.controllers.js"
 
 const router = Router()
 
-router.route("/").get(getHouses)
+router.route("/")
+    .get(getHouses)
+    .post(createHouse)
 
-router.route("/:id").get(getHouseByID)
+
+router.route("/:id")
+    .get(getResidentsByHouseID)
+    .post(createResident)
 
 export default router
