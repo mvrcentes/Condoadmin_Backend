ca8365af9de0e58850e6d6f6f8df30e0fb5e14c9
mvrcentes <mvrcentes@hotmail.com>
2023-09-05 18:02:05 -0600

    Add more test

--- test/admin/adminHouses.spec.js
+++ test/admin/adminHouses.spec.js
@@ -57,3 +57,51 @@ describe("POST /api/houses", () => {
 })
 
 // Puedes agregar más describe y test para otras rutas y funciones.
+
+
+describe('Pruebas básicas', () => {
+    it('Debería sumar correctamente', () => {
+      const resultado = 2 + 2;
+      expect(resultado).toBe(4);
+    });
+  
+    it('Debería verificar que una variable sea verdadera', () => {
+      const esVerdadero = true;
+      expect(esVerdadero).toBeTruthy();
+    });
+  
+    it('Debería verificar que una variable sea falsa', () => {
+      const esFalso = false;
+      expect(esFalso).toBeFalsy();
+    });
+  
+    it('Debería comparar dos cadenas de texto', () => {
+      const cadena1 = 'Hola, mundo';
+      const cadena2 = 'Hola, mundo';
+      expect(cadena1).toBe(cadena2);
+    });
+  
+    it('Debería verificar que un objeto no sea nulo', () => {
+      const objeto = { nombre: 'Ejemplo' };
+      expect(objeto).not.toBeNull();
+    });
+  
+    it('Debería verificar que un arreglo contenga un elemento específico', () => {
+      const arreglo = [1, 2, 3, 4, 5];
+      expect(arreglo).toContain(3);
+    });
+  
+    it('Debería ejecutarse sin errores', () => {
+      // Este es un ejemplo de prueba que siempre pasa porque no tiene expectativas.
+      // Puede ser útil para verificar que una función se ejecute sin errores.
+      const resultado = funcionQueNoArrojaErrores();
+      expect(resultado).toBeDefined();
+    });
+  });
+  
+  // Función de ejemplo que no arroja errores
+  function funcionQueNoArrojaErrores() {
+    // Esta función no hace nada especial, solo devuelve un valor.
+    return 'Ejemplo';
+  }
+  
